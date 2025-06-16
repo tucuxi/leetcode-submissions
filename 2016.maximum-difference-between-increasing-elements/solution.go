2a9ec10bc771a1ce9b3c06f2fdd605814387e47e
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-06-16 07:56:40 +0200

    Add solution for problem #2016

---
+++ 2016.maximum-difference-between-increasing-elements/solution.go
@@ -0,0 +1,11 @@
+func maximumDifference(nums []int) int {
+	res, min := -1, nums[0]
+	for _, n := range nums {
+		if min < n {
+			res = max(res, n-min)
+		} else {
+			min = n
+		}
+	}
+	return res
+}
