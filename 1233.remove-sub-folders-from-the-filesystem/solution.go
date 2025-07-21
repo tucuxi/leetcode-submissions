71422b2771712864b9ebc1c02722a4a0ad7a0a52
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-07-21 22:51:51 +0200

    Update solution for problem #1233

--- 1233.remove-sub-folders-from-the-filesystem/solution.go
+++ 1233.remove-sub-folders-from-the-filesystem/solution.go
@@ -1,8 +1,6 @@
 func removeSubfolders(folder []string) []string {
-    var res []string
-
     slices.Sort(folder)
-    res = append(res, folder[0])
+    res := []string{folder[0]}
     p := folder[0] + "/"
     for _, s := range folder[1:] {
         if !strings.HasPrefix(s, p) {
