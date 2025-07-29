54af32f7cd850111f94f0ab0606dbd61eafd66d6
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-07-29 20:32:21 +0200

    Add solution for problem #3487

--- 3487.maximum-unique-subarray-sum-after-deletion/solution.kt
+++ 3487.maximum-unique-subarray-sum-after-deletion/solution.kt
@@ -1,11 +1,10 @@
 class Solution {
     fun maxSum(nums: IntArray): Int {
-        return nums.partition { it >= 0 }.let { (p, n) ->
-            if (p.isNotEmpty()) {
-                p.toSet().sum()
-            } else {
-                n.max()
-            }
-        }
+        val (nonNeg, neg) = nums.partition { it >= 0 }
+        return if (nonNeg.isNotEmpty()) {
+            nonNeg.distinct().sum()
+         } else {
+            neg.max()
+         } 
     }
 }
