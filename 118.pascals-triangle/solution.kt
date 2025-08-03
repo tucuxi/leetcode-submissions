b16c1a081375ab7b724f7408ede1b49f5362b366
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-08-03 14:01:18 +0200

    Update solution for problem #118

--- 118.pascals-triangle/solution.kt
+++ 118.pascals-triangle/solution.kt
@@ -1,16 +1,10 @@
 class Solution {
-    fun generate(numRows: Int): List<List<Int>> {
-        val triangle = mutableListOf<List<Int>>()
-        for (i in 0 until numRows) {
-            val row = MutableList(i + 1) { j ->
-                if (j == 0 || j == i) {
-                    1
-                } else {
-                    triangle[i - 1][j] + triangle[i - 1][j - 1]
-                }
-            }
-            triangle.add(row)
+    fun generate(numRows: Int): List<List<Int>> =
+        generateSequence(listOf(1)) { row ->
+            List(row.size + 1) { i ->
+                row.getOrElse(i - 1) { 0 } + row.getOrElse(i) { 0 }
+             }
         }
-        return triangle
-    }
+        .take(numRows)
+        .toList()
 }
