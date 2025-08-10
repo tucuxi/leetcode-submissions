43ac4f2919b9f20ca354aa62211793f1ae31b916
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-08-10 09:49:10 +0200

    Add solution for problem #231

--- 231.power-of-two/solution.go
+++ 231.power-of-two/solution.go
@@ -1,9 +1,3 @@
 func isPowerOfTwo(n int) bool {
-    if n <= 0 {
-        return false
-    }
-    for n & 1 == 0 {
-        n >>= 1
-    }
-    return n == 1
+    return n > 0 && n & (n - 1) == 0
 }
--- 231.power-of-two/solution.kt
+++ 231.power-of-two/solution.kt
@@ -1,12 +1,5 @@
 class Solution {
     fun isPowerOfTwo(n: Int): Boolean {
-        if (n <= 0) {
-            return false
-        }
-        var p = n
-        while (p and 1 == 0) {
-            p = p shr 1
-        }
-        return p == 1
+        return n > 0 && (n and n - 1) == 0
     }
 }
