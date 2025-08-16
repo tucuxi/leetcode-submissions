669416a5725f56e46c86725b1efaa28ad4a38be3
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-08-16 18:41:56 +0200

    Update solution for problem #1323

--- 1323.maximum-69-number/solution.kt
+++ 1323.maximum-69-number/solution.kt
@@ -1,4 +1,5 @@
 class Solution {
-    fun maximum69Number(num: Int) =
-        num.toString().replaceFirst("6", "9").toInt()
+    fun maximum69Number (num: Int): Int {
+        return num.toString().replaceFirst('6', '9').toInt()
+    }
 }
