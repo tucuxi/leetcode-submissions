b6b650527759558cf5b3eb77c64b4c0ab40fd0d8
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-08-16 18:17:26 +0200

    Update solution for problem #326

--- 326.power-of-three/solution.go
+++ 326.power-of-three/solution.go
@@ -1,12 +1,3 @@
 func isPowerOfThree(n int) bool {
-    if n <= 0 {
-        return false
-    }
-    for n > 1 {
-        if n % 3 != 0 {
-            return false
-        }
-        n /= 3
-    }
-    return true
+    return n > 0 && 1162261467 % n == 0
 }
--- 326.power-of-three/solution.kt
+++ 326.power-of-three/solution.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun isPowerOfThree(n: Int): Boolean {
-        if (n <= 0) return false
-        var m = n
-        while (m > 1) {
-            if (m % 3 != 0) return false
-            m /= 3
-        }
-        return true
+        return n > 0 && 1162261467 % n == 0
     }
 }
