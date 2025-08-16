200e9d5249c61676da6ea428ee9ffba2b983cf51
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-08-16 18:17:59 +0200

    Update solution for problem #869

--- 869.reordered-power-of-2/solution.kt
+++ 869.reordered-power-of-2/solution.kt
@@ -1,27 +1,19 @@
 class Solution {
     fun reorderedPowerOf2(n: Int): Boolean {
-        val permutations = sequence<Int> {
-            val digits = n.toString().map { ch -> ch - '0' }
-            val visited = BooleanArray(digits.size)
-
-            suspend fun SequenceScope<Int>.dfs(acc: Int, remaining: Int) {
-                if (remaining == 0) {
-                    yield(acc)
-                    return
-                }
-                for (i in digits.indices) {
-                    if (!visited[i] && (digits[i] != 0 || acc != 0)) {
-                        visited[i] = true
-                        dfs(10 * acc + digits[i], remaining - 1)
-                        visited[i] = false
-                    }
-                }
-            }
-
-            dfs(0, digits.size)
+        val digitsOfN = internal(n)
+        return (0..31).any { b ->
+            val candidate = internal(1 shl b)
+            digitsOfN.contentEquals(candidate)
         }
-        return permutations.any { isPowerOf2(it) }
     }
 
-    fun isPowerOf2(n: Int) = n and (n - 1) == 0
+    fun internal(n: Int): IntArray {
+        return IntArray(10).apply {
+            var k = n
+            while (k > 0) {
+                this[k % 10]++
+                k /= 10
+            }
+        }
+    }
 }
