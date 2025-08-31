f4bf13ba6c00784be7796a325b2dd6a7206260e9
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-08-31 10:42:53 +0200

    Update solution for problem #3195

--- 3195.find-the-minimum-area-to-cover-all-ones-i/solution.go
+++ 3195.find-the-minimum-area-to-cover-all-ones-i/solution.go
@@ -2,37 +2,29 @@ func minimumArea(grid [][]int) int {
     minCol, maxCol := len(grid[0]), 0
     for i := range grid {
         j := 0
-        for j < len(grid[i]) && grid[i][j] == 0 {
+        for j < minCol && grid[i][j] == 0 {
             j++
         }
-        if j < minCol {
-            minCol = j
-        }
+        minCol = j
         j = len(grid[i]) - 1
         for j > maxCol && grid[i][j] == 0 {
             j--
         }
-        if j > maxCol {
-            maxCol = j
-        }
+        maxCol = j
     }
 
     minRow, maxRow := len(grid), 0
     for j := range grid[0] {
         i := 0
-        for i < len(grid) && grid[i][j] == 0 {
+        for i < minRow && grid[i][j] == 0 {
             i++
         }
-        if i < minRow {
-            minRow = i
-        }
+        minRow = i
         i = len(grid) - 1
         for i > maxRow && grid[i][j] == 0 {
             i--
         }
-        if i > maxRow {
-            maxRow = i
-        }
+        maxRow = i
     }
 
     return (maxRow - minRow + 1) * (maxCol - minCol + 1)
