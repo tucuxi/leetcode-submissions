3d8dad62389c022410b08884ca6fcb854b058e99
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-09-26 23:39:22 +0200

    Update solution for problem #165

--- 165.compare-version-numbers/solution.kt
+++ 165.compare-version-numbers/solution.kt
@@ -1,18 +1,18 @@
+import java.util.Scanner
+
 class Solution {
     fun compareVersion(version1: String, version2: String): Int {
-        val v1 = Version(version1)
-        val v2 = Version(version2)
-        repeat(maxOf(v1.getLength(), v2.getLength())) { i ->
-            if (v1.getRevision(i) < v2.getRevision(i)) return -1
-            if (v1.getRevision(i) > v2.getRevision(i)) return 1
+        val scanner1 = Scanner(version1).useDelimiter("\\.")
+        val scanner2 = Scanner(version2).useDelimiter("\\.")
+
+        while (scanner1.hasNextInt() || scanner2.hasNextInt()) {
+            val v1 = if (scanner1.hasNextInt()) scanner1.nextInt() else 0
+            val v2 = if (scanner2.hasNextInt()) scanner2.nextInt() else 0
+            when {
+                v1 < v2 -> return -1
+                v1 > v2 -> return 1
+            }
         }
         return 0
     }
-}
-
-class Version(version: String) {
-    val revisions = version.split('.').map { it.toInt() }
-
-    fun getRevision(n: Int) = revisions.getOrElse(n) { 0 }
-    fun getLength() = revisions.size
 }
