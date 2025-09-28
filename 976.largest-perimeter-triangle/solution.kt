0a8ae89473c4def5b918ff984e13a5b0aa98bed4
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-09-28 10:53:25 +0200

    Add solution for problem #976

---
+++ 976.largest-perimeter-triangle/solution.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun largestPerimeter(nums: IntArray): Int {
+        nums.sort()
+        for (i in nums.lastIndex downTo 2) {
+            if (nums[i] < nums[i - 1] + nums[i - 2]) {
+                return nums[i] + nums[i - 1] + nums[i - 2]
+            }
+        }
+        return 0
+    }
+}
