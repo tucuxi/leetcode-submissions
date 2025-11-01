b0202662b24f898c478be53bb82904fd9797e950
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2025-11-01 12:17:03 +0100

    Update solution for problem #3349

--- 3349.adjacent-increasing-subarrays-detection-i/solution.go
+++ 3349.adjacent-increasing-subarrays-detection-i/solution.go
@@ -1,7 +1,7 @@
 func hasIncreasingSubarrays(nums []int, k int) bool {
-    previousSubarray, currentSubarray := 0, 0
-    for i := range nums {
-        if i > 0 && nums[i - 1] >= nums[i] {
+    previousSubarray, currentSubarray := 0, 1
+    for i := 1; i < len(nums); i++ {
+        if nums[i - 1] >= nums[i] {
             if adjacentSubarrays(previousSubarray, currentSubarray, k) {
                 return true
             }
