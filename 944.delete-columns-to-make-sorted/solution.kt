f9adfb354de104e3ffb8e8876cea0603f018e121
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2026-01-24 12:07:44 +0100

    Update solution for problem #944

--- 944.delete-columns-to-make-sorted/solution.kt
+++ 944.delete-columns-to-make-sorted/solution.kt
@@ -1,8 +1,11 @@
 class Solution {
-    fun minDeletionSize(strs: Array<String>): Int =
-        (0 until strs[0].length).count { i ->
-            (0 until strs.lastIndex).any { j ->
-                strs[j][i] > strs[j + 1][i]
+    fun minDeletionSize(strs: Array<String>): Int {
+        val deletedColumns = BooleanArray(strs.first().length)
+        for (i in 1 until strs.size) {
+            for (j in deletedColumns.indices) {
+                deletedColumns[j] = deletedColumns[j] or (strs[i][j] < strs[i - 1][j])
             }
         }
+        return deletedColumns.count { it }
+    }
 }
