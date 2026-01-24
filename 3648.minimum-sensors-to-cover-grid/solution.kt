18fb2f3f5f6a6415e9a8589ca538e29d6c87c0cd
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2026-01-24 13:01:24 +0100

    Add solution for problem #3648

---
+++ 3648.minimum-sensors-to-cover-grid/solution.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun minSensors(n: Int, m: Int, k: Int): Int {
+        val side = 2 * k + 1
+        val rows = (n + side - 1) / side
+        val cols = (m + side - 1) / side
+        return rows * cols
+    }
+}
