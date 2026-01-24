64d1e49dcd2ece34404031a28616710d8cce554a
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2026-01-24 12:08:52 +0100

    Update solution for problem #1266

--- 1266.minimum-time-visiting-all-points/solution.kt
+++ 1266.minimum-time-visiting-all-points/solution.kt
@@ -1,11 +1,9 @@
 class Solution {
     fun minTimeToVisitAllPoints(points: Array<IntArray>): Int {
-        var res = 0
-        for (i in 1 until points.size) {
-            var dx = Math.abs(points[i][0] - points[i - 1][0])
-            var dy = Math.abs(points[i][1] - points[i - 1][1])
-            res += Math.max(dx, dy)
+        return (1 until points.size).sumOf { i ->
+            val dx = abs(points[i - 1][0] - points[i][0])
+            val dy = abs(points[i - 1][1] - points[i][1])
+            maxOf(dx, dy)
         }
-        return res
     }
 }
