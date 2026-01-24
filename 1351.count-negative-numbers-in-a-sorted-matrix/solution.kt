4e6923bb2bdbe3ac8fc51cbff53b53c13824e108
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2026-01-24 12:11:33 +0100

    Update solution for problem #1351

--- 1351.count-negative-numbers-in-a-sorted-matrix/solution.kt
+++ 1351.count-negative-numbers-in-a-sorted-matrix/solution.kt
@@ -1,4 +1,22 @@
 class Solution {
-    fun countNegatives(grid: Array<IntArray>) =
-        grid.fold(0) { acc, elem -> acc + elem.count { it < 0 } }
+    fun countNegatives(grid: Array<IntArray>): Int {
+        return grid.sumOf { row -> row.size - row.strictBinarySearch { it < 0 } }
+    }
+}
+
+// Unlike binarySearch from the standard library, this reports the
+// index of the first match or the insert position.
+fun IntArray.strictBinarySearch(predicate: (Int) -> Boolean): Int {
+    var l = 0
+    var r = size
+
+    while (l < r) {
+        val m = l + (r - l) / 2
+        if (!predicate(this[m])) {
+            l = m + 1
+        } else {
+            r = m
+        }
+    }
+    return r
 }
