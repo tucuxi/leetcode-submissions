155c9518ed36fdebfff638c5224cbc50b7b03a69
Klaus-Dieter Schmatz <klausds@proton.me>
2026-02-07 20:37:51 +0100

    Add solution for problem #1206

---
+++ 1206.design-skiplist/solution.kt
@@ -0,0 +1,84 @@
+const val MAX_LEVEL = 15
+
+class Skiplist() {
+
+    private val sentinel = SkipNode(0, MAX_LEVEL)
+
+    private fun findPredecessors(target: Int): Array<SkipNode<Int>?> {
+        val preds = arrayOfNulls<SkipNode<Int>>(MAX_LEVEL + 1)
+        var node = sentinel
+        
+        for (i in MAX_LEVEL downTo 0) {
+            var nextNode = node.next[i]
+
+            while (nextNode != null && nextNode.value < target) {
+                node = nextNode
+                nextNode = node.next[i]
+            }
+            preds[i] = node
+        }
+        return preds
+    }
+
+    fun search(target: Int): Boolean {
+        var node = sentinel
+        
+        for (i in MAX_LEVEL downTo 0) {
+            var nextNode = node.next[i]
+
+            while (nextNode != null && nextNode.value < target) {
+                node = nextNode
+                nextNode = node.next[i]
+            }
+        }
+        val candidate = node.next[0]
+        return candidate != null && candidate.value == target
+    }
+
+    fun add(num: Int) {
+        val preds = findPredecessors(num)
+        val newLevel = randomLevel()
+        val newNode = SkipNode(num, newLevel)
+
+        for (i in 0..newLevel) {
+            newNode.next[i] = preds[i]!!.next[i]
+            preds[i]!!.next[i] = newNode
+        }
+    }
+
+    fun erase(num: Int): Boolean {
+        val preds = findPredecessors(num)
+        val candidate = preds[0]!!.next[0]
+
+        if (candidate == null || candidate.value != num) {
+            return false
+        }
+        for (i in 0..MAX_LEVEL) {
+            if (preds[i]!!.next[i] != candidate) {
+                break
+            }
+            preds[i]!!.next[i] = candidate.next[i]
+        }
+        return true
+    }
+
+    private fun randomLevel(): Int {
+        var level = 0
+        while (level < MAX_LEVEL && Random.nextBoolean()) {
+            level++
+        }
+        return level
+    }
+}
+
+class SkipNode<T : Comparable<T>>(val value: T, val level: Int) {
+    val next = arrayOfNulls<SkipNode<T>>(level + 1)
+}
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * var obj = Skiplist()
+ * var param_1 = obj.search(target)
+ * obj.add(num)
+ * var param_3 = obj.erase(num)
+ */
