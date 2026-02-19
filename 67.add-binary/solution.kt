eee880a9219e42187da54dab1ff6611d5a200520
Klaus-Dieter Schmatz <klausds@proton.me>
2026-02-19 20:49:00 +0100

    Update solution for problem #67

--- 67.add-binary/solution.kt
+++ 67.add-binary/solution.kt
@@ -1,33 +1,22 @@
-import java.math.BigInteger
-
 class Solution {
     fun addBinary(a: String, b: String): String {
-        val a1 = binaryToBigInteger(a)
-        val b1 = binaryToBigInteger(b)
-        val c = a1.add(b1)
-        return bigIntegerToBinary(c)
-    }
-    
-    fun binaryToBigInteger(a: String): BigInteger =
-        a.fold(BigInteger.ZERO) { res, digit ->
-            val doubled = res.shiftLeft(1)
-            if (digit == '0') doubled else doubled.inc() 
-        }
-        
-    fun bigIntegerToBinary(n: BigInteger): String {
-        if (n == BigInteger.ZERO) {
-            return "0"
-        }
-        val res = StringBuilder()
-        var m = n
-        while (m != BigInteger.ZERO) {
-            if (m.mod(BigInteger("2")) == BigInteger.ZERO) {
-                res.append('0')
-            } else {
-                res.append('1')
+        var i = a.lastIndex
+        var j = b.lastIndex
+        var c = 0
+
+        val reversed = buildString {
+            while (i >= 0 || j >= 0 || c > 0) {
+                if (i >= 0) {
+                    c += a[i--] - '0'
+                }
+                if (j >= 0) {
+                    c += b[j--] - '0'
+                }
+                append('0' + (c and 1))
+                c = c shr 1
             }
-            m = m.shiftRight(1)
         }
-        return res.reverse().toString()
+
+        return reversed.reversed().toString()
     }
 }
