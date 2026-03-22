bd3c992ffcced36e230c542a96b2bb96b7206259
Klaus-Dieter Schmatz <klausds@proton.me>
2026-03-22 12:11:45 +0100

    Update solution for problem #3868

--- 3868.minimum-cost-to-equalize-arrays-using-swaps/solution.kt
+++ 3868.minimum-cost-to-equalize-arrays-using-swaps/solution.kt
@@ -1,21 +1,16 @@
 class Solution {
     fun minCost(nums1: IntArray, nums2: IntArray): Int {
-        val counts1 = nums1.asIterable().groupingBy { it }.eachCount()
-        val totalCounts = counts1.toMutableMap()
+        val counts = mutableMapOf<Int, Int>()
+        
+        nums1.forEach { counts[it] = counts.getOrDefault(it, 0) + 1 }
+        nums2.forEach { counts[it] = counts.getOrDefault(it, 0) - 1 }
 
-        nums2.forEach { totalCounts[it] = totalCounts.getOrDefault(it, 0) + 1 }
+        val v = counts.values
 
-        var res = 0L
-
-        totalCounts.forEach { (num, totalCount) ->
-            if (totalCount % 2 != 0) {
-                return -1
-            }
-            val targetCount = totalCount / 2
-            val count1 = counts1.getOrDefault(num, 0)
-            res += maxOf(0, count1 - targetCount)
+        return if (v.any { it % 2 != 0 }) {
+            -1
+        } else {
+            v.filter { it > 0 }.sum() / 2
         }
-
-        return res.toInt()
     }
 }
