9ac9153098cdd8ead20c221a5020823390b0890b
Klaus-Dieter Schmatz <klausds@proton.me>
2026-03-22 11:43:29 +0100

    Update solution for problem #1886

--- 1886.determine-whether-matrix-can-be-obtained-by-rotation/solution.go
+++ 1886.determine-whether-matrix-can-be-obtained-by-rotation/solution.go
@@ -1,8 +1,8 @@
 func findRotation(mat [][]int, target [][]int) bool {
     n := len(mat)
     excl := [4]bool{}
-    for i := 0; i < n; i++ {
-        for j := 0; j < n; j++ {
+    for i := range n {
+        for j := range n {
             excl[0] = excl[0] || mat[i][j] != target[i][j]
             excl[1] = excl[1] || mat[i][j] != target[j][n - i - 1]
             excl[2] = excl[2] || mat[i][j] != target[n - i - 1][n - j - 1]
