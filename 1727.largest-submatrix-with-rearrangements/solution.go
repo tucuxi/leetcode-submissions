f2b47135a38bcdcbede6df16fbf1bf0faa32a45d
Klaus-Dieter Schmatz <klausds@proton.me>
2026-03-22 11:43:17 +0100

    Update solution for problem #1727

--- 1727.largest-submatrix-with-rearrangements/solution.go
+++ 1727.largest-submatrix-with-rearrangements/solution.go
@@ -6,19 +6,18 @@ func largestSubmatrix(matrix [][]int) int {
     for i := range matrix {
         currRow := make([]int, n)
         copy(currRow, matrix[i])
+        
         for j := range currRow {
             if currRow[j] != 0 {
                 currRow[j] += prevRow[j]
             }
         }
         
-        copy(prevRow, currRow)
-        
-        sort.Ints(currRow)
+        copy(prevRow, currRow)        
+        slices.Sort(currRow)
+
         for j := range currRow {
-            if a := currRow[j] * (n - j); a > res {
-                res = a
-            }
+            res = max(res, currRow[j] * (n - j))
         }
     }
     
