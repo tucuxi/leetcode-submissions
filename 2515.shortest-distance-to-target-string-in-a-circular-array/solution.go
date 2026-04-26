0be2d2cb5f96a1be97f97147a8ca4fc0e248b76d
Klaus-Dieter Schmatz <klausds@proton.me>
2026-04-26 20:28:20 +0200

    Update solution for problem #2515

--- 2515.shortest-distance-to-target-string-in-a-circular-array/solution.go
+++ 2515.shortest-distance-to-target-string-in-a-circular-array/solution.go
@@ -1,7 +1,7 @@
-func closetTarget(words []string, target string, startIndex int) int {
+func closestTarget(words []string, target string, startIndex int) int {
     n := len(words)
-    for i := range words {
-        if words[(startIndex - i + n) % n] == target || words[(startIndex + i) % n] == target{
+    for i := range n/2 + 1 {
+        if words[(startIndex-i+n) % n] == target || words[(startIndex+i) % n] == target {
             return i
         }
     }
