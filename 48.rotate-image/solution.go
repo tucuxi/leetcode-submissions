29e62762a38a58760b797c679b354e0e50c475dc
Klaus-Dieter Schmatz <klausds@proton.me>
2026-05-14 22:38:21 +0200

    Update solution for problem #48

--- 48.rotate-image/solution.go
+++ 48.rotate-image/solution.go
@@ -1,9 +1,9 @@
 func rotate(matrix [][]int)  {
     n := len(matrix)
-    for i := 0; i < n / 2; i++ {
-        k := n - 1 - i
+    for i := range n/2 {
+        k := n-1-i
         for j := i; j < k; j++ {
-            matrix[i][j], matrix[j][k], matrix[k][n - 1 - j], matrix[n - 1 - j][i] = matrix[n - 1 - j][i], matrix[i][j], matrix[j][k], matrix[k][n - 1 - j]
+            matrix[i][j], matrix[j][k], matrix[k][n-1-j], matrix[n-1-j][i] = matrix[n-1-j][i], matrix[i][j], matrix[j][k], matrix[k][n-1-j]
         }
     }
 }
