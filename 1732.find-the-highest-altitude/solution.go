3de80dba90e71478a01efb5b39321e9865cff4fb
Klaus-Dieter Schmatz <klausds@proton.me>
2026-06-21 17:33:54 +0200

    Update solution for problem #1732

--- 1732.find-the-highest-altitude/solution.go
+++ 1732.find-the-highest-altitude/solution.go
@@ -1,10 +1,9 @@
 func largestAltitude(gain []int) int {
-    altitude, highest := 0, 0
+    a := 0
+    m := 0
     for _, g := range gain {
-        altitude += g
-        if altitude > highest {
-            highest = altitude
-        }
+        a += g
+        m = max(a, m)
     }
-    return highest
+    return m
 }
