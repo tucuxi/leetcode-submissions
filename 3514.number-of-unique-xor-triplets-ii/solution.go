5a20816e781a30d67198383b214a0611b128bb16
Klaus-Dieter Schmatz <klausds@proton.me>
2026-07-25 09:00:10 +0200

    Update solution for problem #3514

--- 3514.number-of-unique-xor-triplets-ii/solution.go
+++ 3514.number-of-unique-xor-triplets-ii/solution.go
@@ -1,28 +1,27 @@
 func uniqueXorTriplets(nums []int) int {
-    if n := len(nums); n <= 2 {
-        return n
-    }
-    
-    var pairs [2048]bool
+    var pairs, triplets [2048]bool
+
     for i, a := range nums {
-        for _, b := range nums[i + 1:] {
-            pairs[a ^ b] = true
+        for _, b := range nums[i:] {
+            pairs[a^b] = true
         }
     }
 
-    res := 0
-    var triplets [2048]bool
     for i, p := range pairs {
         if p {
             for _, num := range nums {
-                x := i ^ num
-                if !triplets[x] {
-                    triplets[x] = true
-                    res++
-                }
+                triplets[i^num] = true
             }
         }
     }
 
+    res := 0
+
+    for _, b := range triplets {
+        if b {
+            res++
+        }
+    }
+    
     return res
 }
