0b4e4bc61a474379f74afec4e3a8bcbc3e68a4a7
Klaus-Dieter Schmatz <klausds@proton.me>
2026-08-08 18:25:51 +0200

    Update solution for problem #1464

--- 1464.maximum-product-of-two-elements-in-an-array/solution.go
+++ 1464.maximum-product-of-two-elements-in-an-array/solution.go
@@ -1,4 +1,11 @@
 func maxProduct(nums []int) int {
-    sort.Ints(nums)
-    return (nums[len(nums)-1] - 1) * (nums[len(nums)-2] - 1)
+    m1, m2 := 0, 0
+    for _, num := range nums {
+        if num > m1 {
+            m1, m2 = num, m1
+        } else if num > m2 {
+            m2 = num
+        }
+    }
+    return (m1-1) * (m2-1)
 }
